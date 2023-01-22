8cc4aee5833365041df75efd9d32a3a0b5bee3e2
Russell Coleman <rootofsilence@gmail.com>
2023-01-22 17:22:58 +0000

    added js to colour code time blocks

--- js/script.js
+++ js/script.js
@@ -11,7 +11,34 @@ function displayTime() {
 //Present timeblocks for standard business hours when the user scrolls down.
 
 //Color-code each timeblock based on past, present, and future when the timeblock is viewed.
-
+function colourCode() {
+    let currentHour = moment().format("HH");
+    
+            // loop time blocks
+            $(".time-block").each(function () {
+              var blockHour = parseInt($(this).attr("id"));
+              console.log("c: " + currentHour)
+              console.log("b: " + blockHour)
+    
+              //check against current time
+              if (blockHour < currentHour) {
+                  $(this).addClass("past");
+                  $(this).removeClass("future");
+                  $(this).removeClass("present");
+              }
+              else if (blockHour === currentHour) {
+                  $(this).removeClass("past");
+                  $(this).addClass("present");
+                  $(this).removeClass("future");
+              }
+              else {
+                  $(this).removeClass("present");
+                  $(this).removeClass("past");
+                  $(this).addClass("future");
+              }
+          })
+      }
+      colourCode();
 //Allow a user to enter an event when they click a timeblock.
 
 //Save the event in local storage when the save button is clicked in that timeblock.
